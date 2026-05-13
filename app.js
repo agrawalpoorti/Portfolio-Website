b939e4ef4532999437692ae5372fab23700b22d7
Poorti Agrawal <agrawalpoortiofficial@gmail.com>
2026-05-14 00:06:20 +0530

    Prepare for deployment

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), {
+  maxAge: "1d"
+}));
 
-let port = 8080;
-app.listen(port,(req,res)=>{
-  console.log("Port is listening");
-})
 app.get("/",(req,res)=>{
   res.render("index.ejs");
 })
+
+app.get("/healthz", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.use((req, res) => {
+  res.status(404).redirect("/");
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Portfolio is running on port ${port}`);
+});
